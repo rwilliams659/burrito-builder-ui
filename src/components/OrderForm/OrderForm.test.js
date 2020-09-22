209ce43b67eb64b45ba321eeb6d24e179e9dd0e7
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-22 10:07:27 -0600

    Add OrderForm test for displaying correct UI

---
+++ src/components/OrderForm/OrderForm.test.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import OrderForm from './OrderForm'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Order Form', () => {
+  it('should display the correct information ', () => {
+
+    render (
+      <OrderForm
+        addNewOrder={jest.fn()}
+      />
+    )
+
+    const nameInput = screen.getByPlaceholderText('Name');
+    const beansBtn = screen.getByRole('button', { name: 'beans'});
+    const steakBtn = screen.getByRole('button', { name: 'steak' });
+    const carnitasBtn = screen.getByRole('button', { name: 'carnitas' });
+    const sofritasBtn = screen.getByRole('button', { name: 'sofritas' });
+    const lettuceBtn = screen.getByRole('button', { name: 'lettuce' });
+    const quesoFrescoBtn = screen.getByRole('button', { name: 'queso fresco' });
+    const picoDeGalloBtn = screen.getByRole('button', { name: 'pico de gallo' });
+    const hotSauceBtn = screen.getByRole('button', { name: 'hot sauce' });;
+    const guacamoleBtn = screen.getByRole('button', { name: 'guacamole' });
+    const jalapenosBtn = screen.getByRole('button', { name: 'jalapenos' });
+    const cilantroBtn = screen.getByRole('button', { name: 'cilantro' });
+    const sourCreamBtn = screen.getByRole('button', { name: 'sour cream' });
+    const orderMsg = screen.getByText('Order: Nothing selected');
+    const submitBtn = screen.getByRole('button', {name: 'Submit Order'});
+
+    expect(nameInput).toBeInTheDocument();
+    expect(beansBtn).toBeInTheDocument();
+    expect(steakBtn).toBeInTheDocument();
+    expect(carnitasBtn).toBeInTheDocument();
+    expect(sofritasBtn).toBeInTheDocument();
+    expect(lettuceBtn).toBeInTheDocument();
+    expect(quesoFrescoBtn).toBeInTheDocument();
+    expect(picoDeGalloBtn).toBeInTheDocument();
+    expect(hotSauceBtn).toBeInTheDocument();
+    expect(guacamoleBtn).toBeInTheDocument();
+    expect(jalapenosBtn).toBeInTheDocument();
+    expect(cilantroBtn).toBeInTheDocument();
+    expect(sourCreamBtn).toBeInTheDocument();
+    expect(orderMsg).toBeInTheDocument();
+    expect(submitBtn).toBeInTheDocument();
+  });
+
+  //fill out form, expect form values to be what user selected
+
+  //
+})
