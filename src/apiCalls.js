8543a469c33ce165b81ce0b3f109176655202c95
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-22 10:52:52 -0600

    Add delete fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -19,3 +19,16 @@ export const postOrder = (order) => {
       }
     })
 }
+
+export const deleteOrder = (orderId) => {
+  return fetch(`http://localhost:3001/api/v1/orders${orderId}`, {
+    method: 'DELETE'
+  })
+    .then(response => {
+      if (response.ok) {
+        return 'Successful delete'
+      } else {
+        throw Error (response.statusText)
+      }
+    })
+}
