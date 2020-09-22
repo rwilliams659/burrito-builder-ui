5cb3c3eb8e066210618dafae2eefa0a9836f3e7f
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-22 09:48:59 -0600

    Create addNewOrder function in App to post new order, and pass down to OrderForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {getOrders, postOrder} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
@@ -18,12 +18,24 @@ class App extends Component {
       .catch(err => console.error('Error fetching:', err));
   }
 
+  addNewOrder = (orderToPost) => {
+    postOrder()
+      .then(order => {
+        const newOrderState = this.state.orders;
+        newOrderState.push(order);
+        this.setState({ orders: newOrderState})
+      })
+      .catch(err => console.error('Could not post new order'))
+  }
+
   render() {
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm 
+            addNewOrder={this.addNewOrder}
+          />
         </header>
 
         <Orders orders={this.state.orders}/>
